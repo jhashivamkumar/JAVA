644ef13782dfd64579afa3780772162f34651a81
Isaac <isuntag@gmail.com>
2017-09-03 11:53:01 -0400

    Added string_manipulation project

---
+++ MyJava.java
@@ -0,0 +1,5 @@
+public class MyJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Isaac Suntag.\nI am 24 years old.\nMy hometown is Brattleboro, VT.");
+    }
+}
