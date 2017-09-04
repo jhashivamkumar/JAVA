a52bca981484cac50c59d21a5226929ab5e3a2b2
Isaac <isuntag@gmail.com>
2017-09-04 17:01:38 -0400

    Added object_master project

---
+++ OOP/object_master/src/com/codingdojo/objectmaster/Human.java
@@ -0,0 +1,21 @@
+package com.codingdojo.objectmaster;
+
+public class Human {
+	public int strength;
+	public int stealth;
+	public int intelligence;
+	public int health;
+	public Human() {
+		this.strength = 3;
+		this.stealth = 3;
+		this.intelligence = 3;
+		this.health = 100;
+	}
+	public Human attack(Human attacked) {
+		attacked.health -= this.strength;
+		return this;
+	}
+	public void displayHealth() {
+		System.out.println(this.health);
+	}
+}
---
+++ OOP/object_master/src/com/codingdojo/objectmaster/HumanTest.java
@@ -0,0 +1,12 @@
+package com.codingdojo.objectmaster;
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		Human human = new Human();
+		Human human2 = new Human();
+		human.attack(human2);
+		human2.displayHealth();
+	}
+
+}
