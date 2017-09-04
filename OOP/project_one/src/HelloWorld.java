2fe9c58bd7faee15887e940424c3d6a0bdd05de4
Isaac <isuntag@gmail.com>
2017-09-04 16:08:31 -0400

    Added project_one to test STS

---
+++ OOP/project_one/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+	private int num;
+	public String greet() {
+		return "Hello world!";
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+}
