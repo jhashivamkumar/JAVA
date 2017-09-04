691cad23a8434cfd9a9e2b9b5d80a50d258721e1
Isaac <isuntag@gmail.com>
2017-09-04 19:14:45 -0400

    Added bankaccount project

---
+++ OOP/bank_account/src/com/codingdojo/bankaccount/BankAccountTest.java
@@ -0,0 +1,14 @@
+package com.codingdojo.bankaccount;
+
+public class BankAccountTest {
+
+	public static void main(String[] args) {
+		BankAccount account = new BankAccount();
+		account.deposit("checking", 100);
+		account.deposit("savings", 1000);
+		account.withdraw("checking", 25);
+		account.getBalances();
+
+	}
+
+}
